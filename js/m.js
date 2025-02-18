d13eb286672af6f4d329d6d2e7a38b4889205779
Youssef3_ <46034687+youssefjojo@users.noreply.github.com>
2025-02-18 22:29:09 +0200

    final

---
+++ js/m.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper-container", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+});
