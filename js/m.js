7a0530d356c6b5919b91f6ef78aade017cd11cde
Youssef3_ <46034687+youssefjojo@users.noreply.github.com>
2026-03-04 20:53:01 +0200

    Restructure project: move Next.js app to root, remove old files

--- js/m.js
+++
@@ -1,9 +0,0 @@
-const swiper = new Swiper(".swiper-container", {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    loop: true,
-});
